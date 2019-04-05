291f98ec927bbc1469a462c39da59b56a4cc57a3
Frodo Schering <frodo@frodoschering.nl>
2019-04-05 16:09:19 +0200

    dsa

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ $(document).ready(function() {
 	// Go to about page
 	$("#about").on('click', function() {
 		page = "about";
-		$('#content').load('http://localhost:8888/manyone/about.html', function(){		
+		$('#content').load('https://frodoschering.github.io/about.html', function(){		
 			changeColor("#000", "#464A4F", "#1E2124", "#323539");
 			TweenMax.to("#email-address", 0.5, {autoAlpha: 0});
 			TweenMax.to("#introduction-text", 0.5, {autoAlpha: 0});
@@ -195,7 +195,7 @@ $(document).ready(function() {
 	// Go to work page
 	$("#work").on('click', function() {
 		page = "work";
-		$('#content').load('http://localhost:8888/manyone/work.html', function(){
+		$('#content').load('https://frodoschering.github.io/work.html', function(){
 			changeColor("#000", "#464A4F", "#1E2124", "#323539");	
 			TweenMax.to("#email-address", 0.5, {autoAlpha: 0});		
 			TweenMax.to("#introduction-text", 0.5, {autoAlpha: 0});		
